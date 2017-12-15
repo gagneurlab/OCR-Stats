80125cc908c597c8ef85006edd53bb51d351e507
Vicente <yepez@in.tum.de>
2017-12-15 18:26:14 +0100

    example modified

--- src/r/example.R
+++ src/r/example.R
@@ -1,23 +1,29 @@
 source('src/r/config.R')
 
-
-dt <- fread("/data/ouga/home/ag_gagneur/yepez/LRZ Sync+Share/LMU/paper/fig/table_s3_all_OCR.txt")
+# Read example data
+dt <- fread("data/table_s4_all_OCR.txt")
 setnames(dt, "Cell_line", "Fibroblast_id")
 
+# Find outliers
 dt_ao <- add_outlier_col(dt)
 
+# Compute bioenergetics
 bio_dt <- compute_bioenergetics(dt_ao, methods = "LR_ao")
 
+# Create a comparison table 
 comp_dt <- create_comp_table(dt_ao)
 
-pt <- stat_test_OCR(bio_dt, data.table(s1 = c("71215_20130503_1438", "Fibro_VY_083_20140128_1817"), 
-                                       s2 = c("NHDF_20130503_1438", "NHDF_20140128_1817")))
-
+# Perform statistical testing between samples
 pt <- stat_test_OCR(bio_dt, comp_dt)
+
+# Doesn't have enough replicates across plates to compute pvalues
+pt <- stat_test_OCR(bio_dt, data.table(s1 = c("62343_20140128_1817", "Fibro_VY_083_20140128_1817"), 
+                                       s2 = c("NHDF_20140128_1817", "NHDF_20140128_1817")))
 
 #########################
 #### Plots examples #####
 #########################
+
 # 1. Plot all samples inside one plate
 pi = unique(dt_ao$plate_id)[10]
 sh_plot(dt_ao[plate_id == pi])   # scatterplot
@@ -27,3 +33,4 @@ sh_plot(dt_ao[plate_id == pi], geom = "box")   # boxplot
 dt_ao = separate(dt_ao, well, into = c("row", "col"), sep = 1, remove = F)
 cc = unique(dt_ao$cell_culture)[11]
 outlier_plot(dt_ao, cc)
+
