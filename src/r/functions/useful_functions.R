e2cf294ceda61c73b7d6eec5a932f7d821a59086
Vicente <yepez@in.tum.de>
2018-03-14 16:03:13 +0100

    useful functions changed

--- src/r/functions/useful_functions.R
+++ src/r/functions/useful_functions.R
@@ -4,18 +4,6 @@ if.else = function(test, yes, no){
   if(test){ yes } else no
 }
 
- 
-mean2 = function(x) mean(x, na.rm = T)
-median2 = function(x) median(x, na.rm = T)
-min2 = function(x) min(x, na.rm = T)
-max2 = function(x) max(x, na.rm = T)
-
-sum3 = function(x) if.else(sum(is.na(x)) == length(x), NA, sum(x, na.rm = T)) 
-mean3 = function(x) if.else(sum(is.na(x)) == length(x), NA, mean(x, na.rm = T)) 
-median3 = function(x) if.else(sum(is.na(x)) == length(x), NA, median(x, na.rm = T)) 
-min3 = function(x) if.else(sum(is.na(x)) == length(x), NA, min(x, na.rm = T))
-max3 = function(x) if.else(sum(is.na(x)) == length(x), NA, max(x, na.rm = T))
-
 stderr <- function(x) sqrt(var(x,na.rm=TRUE)/length(na.omit(x)))
 
 # Removes element(s) 'a' from vector 'v'
@@ -57,19 +45,6 @@ ss = function(x){
   sum((x - mean(x, na.rm = T))^2, na.rm = T)
 }
 
-# Split data for crossvalidation
-cv_folds = function(n, folds = 10){
-  split(sample(1:n), rep(1:folds, length = n))
-} 
-
-# Computes the shortest distance between a point with coordinates (x,y) and the main diagonal
-dist_point_line = function(x,y){
-  return((x-y)/sqrt(2))
-}
-
-# Coefficient of variations
-rob_cv = function(x) mad(x, na.rm = T)/median(x, na.rm = T)
-cv = function(x) sd(x, na.rm = T)/mean(x, na.rm = T)
 
 first <- function(x) { head(x, n = 1)[1] }
 fin <- function(x) { tail(x, n = 1)[1] }
@@ -94,19 +69,34 @@ shift <- function(x, n, invert = FALSE, default = NA){
   }
 }
 
-sum4 = function(v){
-  m = mean(c(0, v[5:length(v)]))
-  return(c(v[1]+m, v[2]+m, v[3]+m, v[4]+m))
-}
-
-# Reset par (graph) values
-set_par_default <- function() {
-  dev.new()
-  op <- par(no.readonly = TRUE)
-  dev.off()
-  par(op)
-}
 
 get_duplicated = function(v){
   v[duplicated(v)] %>% unique
+}
+
+## Useful seahorse functions
+get_nhdf_from_cc <- function(cc, DT){
+  pi <- get_plate_from_cc(cc, DT)
+  nh_cc <- unique(DT[plate_id %in% pi & Fibroblast_id == "NHDF", cell_culture])
+  if(length(nh_cc) == 0) return ("NA")
+  return(nh_cc)
+}
+
+get_nhdf_from_fib = function(fib, DT){
+  pi = get_plate_from_fib(fib, DT)
+  unique(DT[plate_id %in% pi & Fibroblast_id == "NHDF", cell_culture])
+}
+
+get_plate_from_cc <- function(cc, DT){
+  if(!cc %in% DT$cell_culture)
+    stop("Sample does not exist.")
+  pi <- unique(DT[cell_culture %in% cc, plate_id])
+  return(pi)
+}
+
+get_plate_from_fib <- function(fib, DT){
+  if(! fib %in% DT$Fibroblast_id)
+    stop("Sample does not exist.")
+  pi <- unique(DT[Fibroblast_id %in% fib, plate_id])
+  return(pi)
 }
